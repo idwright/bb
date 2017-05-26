d529a2d9e3522e90253fe82bd1a29c5383713481
Ian Wright <ian.wright@well.ox.ac.uk>
2017-05-26 13:52:40 +0100

    Change to use command line codegen - better and more recent version than the web based one

--- server/run.sh
+++ server/run.sh
@@ -3,10 +3,10 @@ VIRTUAL_ENV_HOME=.
 if [ ${BUILD} = 1 ]
 then
     virtualenv server-env -p /usr/bin/python3
-    rm -rf python-flask-server
-    unzip python-flask-server-generated.zip
-    cp -pr python-flask-server bb-server
-    sed -i 's/inline_response201/inline_response_201/' bb-server/swagger_server/models/*.py
+    (cd ..;./generate.sh)
+    rm -rf bb-server
+    cp -pr ../python-flask-server bb-server
+    #sed -i 's/inline_response201/inline_response_201/' bb-server/swagger_server/models/*.py
 fi
 if [ -f ${VIRTUAL_ENV_HOME}/server-env/bin/activate ]
 then
