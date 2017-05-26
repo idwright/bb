267088b4837fdd1c5b254fcd65a9cf925693c61c
Ian Wright <ian.wright@well.ox.ac.uk>
2017-05-26 10:47:24 +0100

    Fixes

--- server/run.sh
+++ server/run.sh
@@ -1,15 +1,21 @@
-BUILD=0
+BUILD=1
 VIRTUAL_ENV_HOME=.
-VIRTUAL_ENV_HOME=/home/iwright/git/bb-local
 if [ ${BUILD} = 1 ]
 then
     virtualenv server-env -p /usr/bin/python3
+    rm -rf python-flask-server
     unzip python-flask-server-generated.zip
     cp -pr python-flask-server bb-server
+    sed -i 's/inline_response201/inline_response_201/' bb-server/swagger_server/models/*.py
 fi
-source ${VIRTUAL_ENV_HOME}/server-env/bin/activate
-pip3 install -r bb-server/requirements.txt
-cd bb-server
-export PYTHONPATH=$(pwd):${PYTHONPATH}
-echo "http://localhost:8080/v1/ui/"
-python3 -m swagger_server
+if [ -f ${VIRTUAL_ENV_HOME}/server-env/bin/activate ]
+then
+    source ${VIRTUAL_ENV_HOME}/server-env/bin/activate
+    pip3 install -r bb-server/requirements.txt
+    pip3 install -r backbone_server/REQUIREMENTS
+    cp -pr overlay/* bb-server
+    export PYTHONPATH=$(pwd):${PYTHONPATH}
+    cd bb-server
+    echo "http://localhost:8080/v1/ui/"
+    python3 -m swagger_server
+fi
