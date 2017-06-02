0ad42359f2834210d6d7d4070340ec3487be8873
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-02 15:34:34 +0100

    Multiple uploads at the same time

---
+++ server/upload/all_upload.sh
@@ -0,0 +1,20 @@
+SPLIT_SIZE=1000
+
+
+./oxford_central_samples_upload.sh
+
+for SOURCE in sanger_ega_dataset vw_vrpipe pf_6_metadata pv_3_sanger_source_code_metadata pv_3_broad_metadata pv_3_broad_sra_accessions pv_3_locations pv_3_chapellhill_sra_accessions pv_3_sanger_source_code_metadata_test
+do
+    SOURCE=sanger_ega_dataset
+    split -l ${SPLIT_SIZE} ${SOURCE}.txt ${SOURCE}_split_
+
+    for i in ${SOURCE}_split_*
+    do
+        time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@${SOURCE}.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
+        if [ $? -eq 0 ]
+        then
+            rm ${i}
+        fi
+    done
+done
+
--- server/upload/oxford_central_samples_upload.sh
+++ server/upload/oxford_central_samples_upload.sh
@@ -1,7 +1,39 @@
-split -l 1000 oxford_central_samples.csv oxford_central_samples_split_
+SPLIT_SIZE=1000
 
-for i in oxford_central_samples_split_aa
+SOURCE=oxford_central_donor
+split -l ${SPLIT_SIZE} virtual_sample_donor_table_oxford_02JUN2017.csv ${SOURCE}_split_
+
+for i in ${SOURCE}_split_*
 do
-curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_samples.json;type=text/json" -F dataFile="@${i};type=text/csv" 'http://localhost:8080/v1/source/oxford_central_samples/upload' 
+    time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_samples.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
+    if [ $? -eq 0 ]
+    then
+        rm ${i}
+    fi
 done
-rm oxford_central_samples_split_*
+
+SOURCE=oxford_central_donor_parent
+split -l ${SPLIT_SIZE} virtual_donor_parent_mother_table_oxford_02JUN2017.csv ${SOURCE}_split_
+
+for i in ${SOURCE}_split_*
+do
+    time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_samples.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
+    if [ $? -eq 0 ]
+    then
+        rm ${i}
+    fi
+done
+
+SOURCE=oxford_central_samples
+split -l ${SPLIT_SIZE} legacy_samples_table_oxford_02JUN2017.csv ${SOURCE}_split_
+
+for i in ${SOURCE}_split_*
+do
+    time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_samples.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
+    if [ $? -eq 0 ]
+    then
+        rm ${i}
+    fi
+done
+
+
