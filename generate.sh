3ec889464c95cd943b380101994a5a830fbb9c84
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-02 15:36:31 +0100

    Bug fix work around

--- generate.sh
+++ generate.sh
@@ -4,5 +4,5 @@ java -jar swagger-codegen-cli.jar generate -i swagger.yaml -l python-flask -o py
 rm -rf server/bb-server
 cp -pr python-flask-server server/bb-server
 java -jar swagger-codegen-cli.jar generate -i swagger.yaml -l typescript-angular2 -o typescript-angular2-client
-sed -i -e 's/models\.Array/Array/' typescript-angular2-client/model/Entities.ts
+sed -i -e 's/models\.Array/Array/' typescript-angular2-client/model/Entities.ts typescript-angular2-client/model/Summary.ts
 
