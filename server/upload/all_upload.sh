8df3b0af176356ea4e422ec51d8c143515f93347
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-03 19:20:38 +0100

    Fix source mistake

--- server/upload/all_upload.sh
+++ server/upload/all_upload.sh
@@ -5,7 +5,6 @@ SPLIT_SIZE=1000
 
 for SOURCE in sanger_ega_dataset vw_vrpipe pf_6_metadata pv_3_sanger_source_code_metadata pv_3_broad_metadata pv_3_broad_sra_accessions pv_3_locations pv_3_chapellhill_sra_accessions pv_3_sanger_source_code_metadata_test
 do
-    SOURCE=sanger_ega_dataset
     split -l ${SPLIT_SIZE} ${SOURCE}.txt ${SOURCE}_split_
 
     for i in ${SOURCE}_split_*
