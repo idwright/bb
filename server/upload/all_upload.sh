9124206bace6cbc62f249de73afa432a0fc3c3c6
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-05 12:56:52 +0100

    Add VOBS

--- server/upload/all_upload.sh
+++ server/upload/all_upload.sh
@@ -3,7 +3,7 @@ SPLIT_SIZE=1000
 
 ./oxford_central_samples_upload.sh
 
-for SOURCE in sanger_ega_dataset vw_vrpipe pf_6_metadata pv_3_sanger_source_code_metadata pv_3_broad_metadata pv_3_broad_sra_accessions pv_3_locations pv_3_chapellhill_sra_accessions pv_3_sanger_source_code_metadata_test
+for SOURCE in sanger_ega_dataset vw_vrpipe pf_6_metadata pv_3_sanger_source_code_metadata pv_3_broad_metadata pv_3_broad_sra_accessions pv_3_locations pv_3_chapellhill_sra_accessions pv_3_sanger_source_code_metadata_test VOBS
 do
     split -l ${SPLIT_SIZE} ${SOURCE}.txt ${SOURCE}_split_
 
