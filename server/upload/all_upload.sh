6960374e780ab9525d956af4e7f383c62232b9ba
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-13 10:38:54 +0100

    Only upload oxford and roma sources

--- server/upload/all_upload.sh
+++ server/upload/all_upload.sh
@@ -3,12 +3,14 @@ SPLIT_SIZE=1000
 
 ./oxford_central_samples_upload.sh
 
-for SOURCE in sanger_ega_dataset vw_vrpipe pf_6_metadata pv_3_sanger_source_code_metadata pv_3_broad_metadata pv_3_broad_sra_accessions pv_3_locations pv_3_chapellhill_sra_accessions pv_3_sanger_source_code_metadata_test VOBS
+for SOURCE in VOBS genre spotmalaria vivax
+#    sanger_ega_dataset vw_vrpipe pf_6_metadata pv_3_sanger_source_code_metadata pv_3_broad_metadata pv_3_broad_sra_accessions pv_3_locations pv_3_chapellhill_sra_accessions pv_3_sanger_source_code_metadata_test
 do
     split -l ${SPLIT_SIZE} ${SOURCE}.txt ${SOURCE}_split_
 
     for i in ${SOURCE}_split_*
     do
+        echo ${i}
         time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@${SOURCE}.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
         if [ $? -eq 0 ]
         then
