3243d028c8af4ca0f09591b056386c72b147d137
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-14 14:57:59 +0100

    Add simple test for retrieving entity

--- server/client-test/test_source.py
+++ server/client-test/test_source.py
@@ -42,7 +42,6 @@ class TestSource(unittest.TestCase):
 
         try:
                 response = api_instance.upload_entity(self._testSource, entity)
-                print(repr(response))
         except ApiException as e:
             print (repr(entity))
             print("Exception when calling EntityApi->upload_entity: %s\n" % e)
@@ -57,10 +56,39 @@ class TestSource(unittest.TestCase):
         # create an instance of the API class
         api_instance = swagger_client.SourceApi()
         entity = swagger_client.SourceEntity()
-        #data_file = open('test_records', 'r')
         try:
             response = api_instance.upload_source('bulk_test', 'test_records', additional_metadata='test_metadata.json')
-            print(repr(response))
+        except ApiException as e:
+            print (repr(entity))
+            print("Exception when calling EntityApi->upload_entity: %s\n" % e)
+
+    """
+    """
+    def test_fetch_simple_entity(self):
+
+        # create an instance of the API class
+        api_instance = swagger_client.SourceApi()
+        entity = swagger_client.SourceEntity()
+
+        test_id = '1'
+        try:
+            response = api_instance.download_source_entity('test', test_id)
+            self.assertEqual(str(type(response)),"<class 'swagger_client.models.entity.Entity'>")
+            found = False
+            for prop in response.values:
+                if (prop.source == self._testSource):
+                    if prop.identity:
+                        found = True
+                        self.assertEqual(prop.data_value, test_id)
+                        self.assertEqual(prop.data_name, 'id')
+                        self.assertEqual(prop.data_type, 'integer')
+
+            self.assertTrue(found)
+
+
+            #Look for a non-existent entity
+            response = api_instance.download_source_entity('test', '123456789')
+            self.assertIsNone(response)
         except ApiException as e:
             print (repr(entity))
             print("Exception when calling EntityApi->upload_entity: %s\n" % e)
