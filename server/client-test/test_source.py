20f4f233df500dd41e4febfa500ce7814a41f73f
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-15 14:38:11 +0100

    Test creating associations and implied sources

--- server/client-test/test_source.py
+++ server/client-test/test_source.py
@@ -76,6 +76,18 @@ class TestSource(TestBase):
             print (repr(entity))
             fail("Exception when calling EntityApi->upload_entity: %s\n" % e)
 
+    def find_entity(self, api_instance, source, entity_id):
+
+            response = api_instance.download_source_entity(source, entity_id)
+            self.assertEqual(str(type(response)),"<class 'swagger_client.models.entity.Entity'>")
+            found = False
+            for prop in response.values:
+                if prop.identity and prop.source == source and prop.data_value == entity_id:
+                    found = True
+
+            self.assertTrue(found, "Did not find example")
+
+            return response
     """
     """
     def test_fetch_simple_entity(self):
@@ -87,15 +99,8 @@ class TestSource(TestBase):
         test_id = self._example_id_prop.data_value
 
         try:
-            response = api_instance.download_source_entity('test', test_id)
-            self.assertEqual(str(type(response)),"<class 'swagger_client.models.entity.Entity'>")
-            found = False
-            for prop in response.values:
-                if (prop == self._example_id_prop):
-                    found = True
 
-            self.assertTrue(found, "Did not find example")
-
+            self.find_entity(api_instance, 'test', test_id)
 
             #Look for a non-existent entity
             with self.assertRaises(Exception) as context:
@@ -105,4 +110,70 @@ class TestSource(TestBase):
         except ApiException as e:
             print (repr(entity))
             fail("Exception when calling EntityApi->upload_entity: %s\n" % e)
+
+    def check_implied_target(self, api_instance, source_source, source_id, target_entity, implied):
+
+            source_entity = self.find_entity(api_instance, source_source, source_id)
+
+            for prop in source_entity.values:
+                if prop.source == 'system' and prop.data_name == 'implied_id':
+                    self.assertEqual(prop.data_value == 'true', implied)
+
+            found = False
+            for assoc in source_entity.refs:
+                if assoc.source_id == source_entity.entity_id and \
+                   assoc.target_id == target_entity.entity_id:
+                    found = True
+
+            self.assertTrue(found, "Association not found")
+
+            found = False
+            for assoc in target_entity.refs:
+                if assoc.source_id == source_entity.entity_id and \
+                   assoc.target_id == target_entity.entity_id:
+                    found = True
+
+            self.assertTrue(found, "Association not found")
+
+    """
+    """
+    def test_load_source_entities(self):
+
+        # create an instance of the API class
+        api_instance = swagger_client.SourceApi()
+        entity = swagger_client.SourceEntity()
+        try:
+            response = api_instance.upload_source('test_source', 'test_source.txt', additional_metadata='test_source.json')
+
+            rec1 = self.find_entity(api_instance, 'test_source', '3288_6_nonhuman.bam')
+            self.check_implied_target(api_instance, 'test_target', 'PH0042-C', rec1, True)
+            self.check_implied_target(api_instance, 'alfresco', '1011-PF-KH-SU', rec1, True)
+
+            rec2 = self.find_entity(api_instance, 'test_source', '5380_2_nonhuman.bam')
+            self.check_implied_target(api_instance, 'alfresco', '1010-PF-TH-ANDERSON', rec2, True)
+            self.check_implied_target(api_instance, 'test_target', 'PD0123-C', rec2, True)
+
+        except ApiException as e:
+            fail("Exception when calling EntityApi->load_source_entities: %s\n" % e)
+
+    """
+    """
+    def test_load_target_entities(self):
+
+        # create an instance of the API class
+        api_instance = swagger_client.SourceApi()
+        entity = swagger_client.SourceEntity()
+        try:
+            response = api_instance.upload_source('test_target', 'test_target.txt', additional_metadata='test_target.json')
+
+            rec1 = self.find_entity(api_instance, 'test_source', '3288_6_nonhuman.bam')
+            self.check_implied_target(api_instance, 'test_target', 'PH0042-C', rec1, False)
+            self.check_implied_target(api_instance, 'alfresco', '1011-PF-KH-SU', rec1, True)
+
+            rec2 = self.find_entity(api_instance, 'test_source', '5380_2_nonhuman.bam')
+            self.check_implied_target(api_instance, 'test_target', 'PD0123-C', rec2, False)
+            self.check_implied_target(api_instance, 'alfresco', '1010-PF-TH-ANDERSON', rec2, True)
+
+        except ApiException as e:
+            fail("Exception when calling EntityApi->load_source_entities: %s\n" % e)
 
