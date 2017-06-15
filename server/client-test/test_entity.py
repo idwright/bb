fa9070f7e4a6d074a69590e9b906c7574edb144f
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-15 16:53:09 +0100

    Add test to make sure property updates only affect one entity

--- server/client-test/test_entity.py
+++ server/client-test/test_entity.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import unittest
 import time
 import json
+from copy import copy,deepcopy
 import swagger_client
 from swagger_client.rest import ApiException
 from pprint import pprint
@@ -95,8 +96,8 @@ class TestEntity(TestBase):
 
             original = api_instance.download_source_entity('test', test_id)
             entity_id = original.entity_id
-            new_entity = original
-            new_prop = self.get_test_prop()
+            new_entity = deepcopy(original)
+            new_prop = copy(self.get_test_prop())
 
             updated_value = 'Updated value'
             for prop in new_entity.values:
@@ -119,6 +120,60 @@ class TestEntity(TestBase):
 
         except ApiException as e:
             self.fail("Exception when calling EntityApi->update_entity: %s\n" % e)
+
+
+
+    def test_update_property_multi_reference(self):
+       # create an instance of the API class
+        api_instance = swagger_client.SourceApi()
+        entity_api = swagger_client.EntityApi()
+        entity = swagger_client.SourceEntity()
+
+        props = []
+        id_prop = copy(self._example_id_prop)
+        id_prop.data_value = '2'
+        props.append(id_prop)
+        new_prop = copy(self.get_test_prop())
+        props.append(new_prop)
+
+        entity.values = props
+
+        test_id = self._example_id_prop.data_value
+        try:
+            #Check that there's another entity with new_prop as a value
+            other_entity = api_instance.download_source_entity(self._testSource, test_id)
+            found = False
+            for prop in other_entity.values:
+                if (prop == new_prop):
+                    found = True
+
+            api_instance.upload_entity(self._testSource, entity)
+
+            new_entity = api_instance.download_source_entity(self._testSource, id_prop.data_value)
+            updated_value = 'Updated value'
+
+            found = False
+            for prop in new_entity.values:
+                if (prop == new_prop):
+                    found = True
+                    prop.data_value = updated_value
+            self.assertTrue(found, "did not find updated property")
+
+            #Replace the value of new_prop on the entity we just created
+            response = entity_api.update_entity(new_entity.entity_id, new_entity)
+
+            #Check that new_prop on the other_entity hasn't had it's value changed
+            other_entity = api_instance.download_source_entity(self._testSource, test_id)
+            found = False
+            for prop in other_entity.values:
+                if (prop == new_prop):
+                    found = True
+
+            self.assertTrue(found, "Property on original not found")
+        except ApiException as e:
+            print (repr(e))
+            print("Exception when calling EntityApi->upload_entity: %s\n" % e)
+            self.fail("Failed {} {} {}\n".format(e.status, e.reason, e.body))
 
     """
     """
