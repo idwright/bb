03ded875d4b590095e8eb257a5be0dbc7ce3f13a
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-20 16:17:38 +0100

    Drop down for sources

--- client/backbone-client/src/app/entities/entities.component.ts
+++ client/backbone-client/src/app/entities/entities.component.ts
@@ -2,24 +2,40 @@ import { Component, OnInit } from '@angular/core';
 
 import { Entities } from '../typescript-angular2-client/model/Entities';
 import { Entity } from '../typescript-angular2-client/model/Entity';
-import { EntityApi } from '../typescript-angular2-client/api/EntityApi';
+import { Summary } from '../typescript-angular2-client/model/Summary';
+import { ReportApi } from '../typescript-angular2-client/api/ReportApi';
+
 @Component({
 selector: 'my-entities',
-providers: [ EntityApi ],
+providers: [ ReportApi ],
 templateUrl: './entities.component.html',
 
 })
-export class EntitiesComponent  implements OnInit { 
+export class EntitiesComponent implements OnInit, OnChanges { 
 
     sourceName: string;
 
+    sources: Summary;
     propertyName: string;
+
     propertyValue: string;
     
-    constructor(private entityApi: EntityApi) { }
+    constructor(private reportApi: ReportApi) { }
 
 
     ngOnInit(): void {
+
+		 this.reportApi.getSummary()
+			 .subscribe(
+				 (summary) => {
+								 console.log(summary);
+								 this.sources = summary;
+							 },
+				 (err) => console.log(err),
+				 () => { console.log("Downloaded summary") }
+			 );
+
+
     }
 
 }
