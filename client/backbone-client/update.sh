76266943488c89f01baf9214d1d9f6271d022c68
Ian Wright <ian.wright@well.ox.ac.uk>
2017-06-21 14:17:32 +0100

    Script for updating versions

---
+++ client/backbone-client/update.sh
@@ -0,0 +1,9 @@
+#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+npm install @angular/{common,compiler,compiler-cli,core,forms,http,material,platform-browser,platform-browser-dynamic,router,animations}@latest typescript@latest @swimlane/ngx-datatable@latest --save
+npm install --save-dev @angular/cli@latest
+rm package-lock.json
+ng serve
+rm -rf node_modules/
+npm install
+
