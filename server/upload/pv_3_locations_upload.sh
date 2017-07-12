3bddb7feba4541eed8b49f829de62ba2fd814d5d
Ian Wright <ian.wright@well.ox.ac.uk>
2017-07-12 10:50:59 +0100

    add skipHeader option

--- server/upload/pv_3_locations_upload.sh
+++ server/upload/pv_3_locations_upload.sh
@@ -1,1 +1,1 @@
-curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@pv_3_locations.json;type=text/json" -F dataFile="@pv_3_locations.txt;type=text/csv" 'http://localhost:8080/v1/source/pv_3_locations/upload' 
+curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@pv_3_locations.json;type=text/json" -F dataFile="@pv_3_locations.txt;type=text/csv" 'http://localhost:8080/v1/source/pv_3_locations/upload?skipHeader=true' 
