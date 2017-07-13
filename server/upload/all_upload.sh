d29d2309112a7fa8ae594493db9a011ca87d98f8
Ian Wright <ian.wright@well.ox.ac.uk>
2017-07-13 12:08:31 +0100

    changes to reflect changes in the server

--- server/upload/all_upload.sh
+++ server/upload/all_upload.sh
@@ -4,14 +4,22 @@ SPLIT_SIZE=1000
 ./oxford_central_samples_upload.sh
 
 for SOURCE in vobs genre spotmalaria vivax
-#    sanger_ega_dataset vw_vrpipe pf_6_metadata pv_3_sanger_source_code_metadata pv_3_broad_metadata pv_3_broad_sra_accessions pv_3_locations pv_3_chapellhill_sra_accessions pv_3_sanger_source_code_metadata_test
+    #vw_vrpipe sanger_ega_dataset pf_6_metadata pv_3_sanger_source_code_metadata pv_3_broad_metadata pv_3_broad_sra_accessions pv_3_locations pv_3_chapellhill_sra_accessions pv_3_sanger_source_code_metadata_test
 do
     split -l ${SPLIT_SIZE} ${SOURCE}.txt ${SOURCE}_split_
 
+    SKIP='?skipHeader=true'
     for i in ${SOURCE}_split_*
     do
         echo ${i}
-        time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@${SOURCE}.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
+        if [ ${SOURCE} = 'sanger_ega_dataset' ]
+        then
+            UPDATE_ONLY='&updateOnly=true'
+        else
+            UPDATE_ONLY=
+        fi
+        time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@${SOURCE}.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload${SKIP}${UPDATE_ONLY}"
+        SKIP='?skipHeader=false'
         if [ $? -eq 0 ]
         then
             rm ${i}
--- server/upload/oxford_central_samples_upload.sh
+++ server/upload/oxford_central_samples_upload.sh
@@ -4,10 +4,12 @@ if [ "$1" = 'all' ]
 then
     SOURCE=oxford_central_donor_parent
     split -l ${SPLIT_SIZE} virtual_donor_parent_mother_table_oxford_02JUN2017.csv ${SOURCE}_split_
+SKIP=true
 
     for i in ${SOURCE}_split_*
     do
-        time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_samples.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
+        time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_samples.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload?skipHeader=${SKIP}"
+    SKIP=false
         if [ $? -eq 0 ]
         then
             rm ${i}
@@ -16,10 +18,12 @@ then
 
     SOURCE=oxford_central_donor
     split -l ${SPLIT_SIZE} virtual_sample_donor_table_oxford_02JUN2017.csv ${SOURCE}_split_
+SKIP=true
 
     for i in ${SOURCE}_split_*
     do
-        time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_donor.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
+        time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_donor.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload?skipHeader=${SKIP}"
+    SKIP=false
         if [ $? -eq 0 ]
         then
             rm ${i}
@@ -29,10 +33,11 @@ fi
 
 SOURCE=oxford_central_samples
 split -l ${SPLIT_SIZE} legacy_samples_table_oxford_02JUN2017.csv ${SOURCE}_split_
-
+SKIP=true
 for i in ${SOURCE}_split_*
 do
-    time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_samples.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload"
+    time curl --header 'Content-Type: multipart/form-data' --header 'Accept: application/json' -F additionalMetadata="@oxford_central_samples.json;type=text/json" -F dataFile="@${i};type=text/csv" "http://localhost:8080/v1/source/${SOURCE}/upload?skipHeader=${SKIP}"
+    SKIP=false
     if [ $? -eq 0 ]
     then
         rm ${i}
