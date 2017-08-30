745f47cf3c8692e739f39f9087ac18aee22468e4
Ian Wright <ian.wright@well.ox.ac.uk>
2017-08-30 11:32:20 +0100

    Update versions

--- client/backbone-client/update.sh
+++ client/backbone-client/update.sh
@@ -1,6 +1,8 @@
 #curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 #sudo apt-get install -y nodejs
-npm install @angular/{common,compiler,compiler-cli,core,forms,http,material,platform-browser,platform-browser-dynamic,router,animations}@latest typescript@latest @swimlane/ngx-datatable@latest --save
+npm install
+@angular/{common,compiler,compiler-cli,core,forms,http,material,platform-browser,platform-browser-dynamic,router,animations}@latest
+typescript@latest @swimlane/ngx-datatable@latest rxjs@latest --save
 npm install --save-dev @angular/cli@latest
 rm package-lock.json
 ng serve
