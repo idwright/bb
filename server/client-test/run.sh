05128c884d8765b5a64b98269a30841b3980e4eb
Ian Wright <ian.wright@well.ox.ac.uk>
2017-09-08 10:43:18 +0100

    Allow for running a single test

--- server/client-test/run.sh
+++ server/client-test/run.sh
@@ -7,5 +7,9 @@ then
     pip3 install -r ../../python_client/requirements.txt
 fi
 source client-env/bin/activate
-python3 all_tests.py
-#python3 -m unittest test_source.TestSource.test_load_source_entities
+if [ "$1" = "one" ]
+then
+    python3 -m unittest test_upload.TestUpload.test_load_source_entities
+else
+    python3 all_tests.py
+fi
